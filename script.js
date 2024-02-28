bbd61282cddd4ae1dbcd1437818cde0e80865f70
João <joaog.alves14@gmail.com>
2024-02-28 08:41:36 -0300

    Added first cases

--- script.js
+++ script.js
@@ -8,15 +8,19 @@ let cid = [
   ["FIVE", 55],
   ["TEN", 20],
   ["TWENTY", 60],
-  ["ONE HUNDRED", 100]
+  ["ONE HUNDRED", 100],
 ];
 
-const cashInput = document.getElementById("cash")
-const changeDue = document.getElementById("change-due")
-const purchaseButton = document.getElementById("purchase-btn")
+const cashInput = document.getElementById("cash");
+const changeDue = document.getElementById("change-due");
+const purchaseButton = document.getElementById("purchase-btn");
 
 purchaseButton.addEventListener("click", () => {
   if (cashInput.value < 1.87) {
-    alert("Customer does not have enough money to purchase the item")
+    alert("Customer does not have enough money to purchase the item");
+    return;
   }
-})
+  if (parseInt(cashInput.value) === 1.87) {
+    changeDue.innerText = "No change due - customer paid with exact cash"
+  }
+});
